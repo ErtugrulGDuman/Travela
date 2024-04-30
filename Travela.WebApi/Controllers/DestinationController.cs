fd273886aee358b1999c9874f230a268b54e8df4
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-04-30 22:15:39 +0300

    Created Api transactions

---
+++ Travela.WebApi/Controllers/DestinationController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Travela.BusinessLayer.Abstract;
+using Travela.EntityLayer.Concrete;
+
+namespace Travela.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DestinationController : ControllerBase
+    {
+        private readonly IDestinationService _destinationService;
+        public DestinationController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        [HttpGet]
+        public IActionResult DestinationList()
+        {
+            var values = _destinationService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateDestination(Destination destination)
+        {
+            _destinationService.TInsert(destination);
+            return Ok("Rota başarıyla eklendi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteDestination(int id)
+        {
+            _destinationService.TDelete(id);
+            return Ok("Rota başarıyla silindi");
+        }
+        [HttpPut]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            _destinationService.TUpdate(destination);
+            return Ok("Rota başarıyla güncellendi");
+        }
+    }
+}
